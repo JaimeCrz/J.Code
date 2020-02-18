a32ce100f25ba09d21eada2bac637ecd03edad64
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-18 17:25:01 +0100

    Contact me done, test missing due trying to implement side bar

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
           <div className="ui container">
-            <p id="footer">Made with React {React.version}</p>
+            <p id="footer">Made by Jaime</p>
           </div>
         </footer>
       );
