a2cef7a9c53168494b0a8edc045a8d87bfa078fc
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-19 12:17:40 +0100

    Contact me form added, skill page added, test due later

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form, Input, TextArea, Button,} from 'semantic-ui-react'
  
 
 
@@ -7,8 +8,40 @@ const Contact = () => {
         <div className="ui main container">
           <div class="ui stackable two column grid">
 
-            <div class="column">           
-                        
+            <div class="column">
+
+            <Form inverted data-netlify="true" >
+                <Form.Group widths='equal'>
+                  <Form.Field
+                    id='form-input-control-first-name'
+                    control={Input}
+                    label='Name'
+                    placeholder='Name'
+                  />
+                </Form.Group>
+                <Form.Field
+                  id='form-textarea-control-opinion'
+                  control={TextArea}
+                  label='Message'
+                  placeholder='Message'
+                />
+                <Form.Field
+                  id='form-input-control-error-email'
+                  control={Input}
+                  label='Email'
+                  placeholder='email@mail.com'
+                  error={{
+                    content: 'Please enter a valid email address',
+                    pointing: 'below',
+                  }}
+                />
+                <Form.Field
+                  id='form-button-control-public'
+                  control={Button}
+                  content='Confirm'
+                />
+              </Form>   
+                 
             </div>
 
             <div class="column">
