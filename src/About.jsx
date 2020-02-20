a458a24cf91742b7ddfe161e9ad4f95c13cf9372
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-20 12:08:38 +0100

    Updated About/skills and added new css style

--- src/About.jsx
+++ src/About.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { UndrawDesignerLife } from "react-undraw-illustrations";
+import { GridColumn } from "semantic-ui-react";
 
 
 const About = () => {
-    return (
-        <div className="ui main container">
-          <div className="ui stackable two column grid">
-            <div className="column">
-              <UndrawDesignerLife primaryColor='#12283a' />
-            </div>
-            <div className="column">
-              <h1 className="ui white inverted header">About</h1>
-              <p>
-                Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-                corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-                molestias?
-              </p>
-            </div>
+    return (  
+
+
+
+      <div className="ui center aligned grid">
+       <div className="six wide column">       
+        <div className="row">
+          <div className="column">
+          <h1 className="ui white inverted header">About me</h1>
+                <p>
+                My name is Jaime Cruz, a newcomer to the realm of coding and web development, 
+                living in Fristad(Sweden).
+                </p>
+                <p>
+                Thanks to Craft Academy Bootcamp for introducing me
+                to full-stack development.              
+                </p>
+                <p>
+                I'm an organized person, problem solver aficionado, 
+                also a fan of Computer hardware as well as outdoor activities.
+                English speaker along with Spanish/Swedish and few others.
+                </p>  
+                <p>
+                Interested in the entire spectrum of web development and AI Technologies.
+                </p>
           </div>
-        </div>
+
+          <div className="column">
+            <h1 className="ui white inverted header">Image test here</h1>
+                         
+          </div>
+        </div>          
+       </div>
+      </div>         
       );
 };
 
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -2,22 +2,36 @@ import React from 'react'
 
 const Skills = () => {
     return (
-        <div className="ui main container">
-          <div className="ui stackable two column grid">
+      <div className="ui center aligned grid">
+      <div className="six wide column">       
+       <div className="row">
+         <div className="column">
+         <h1 className="ui white inverted header">Skills</h1>
+              
+         </div>
 
-            <div className="column">
+         <div className="column">
+           <h1 className="ui white inverted header">Image test here</h1>
+           
 
+            
+            <div class="dropping-texts">
+              Working with:
+              <div> Ruby</div>
+              <div> Ruby on Rails</div>
+              <div> JavaScript</div>
+              <div> HTML</div>
+              <div> CSS</div>
+              <div> Python</div>
+              <div> Django</div>               
             </div>
 
-            <div className="column">
-            <h1 className="ui white inverted header">Skills</h1>
-              <p>
-              Skills
-              </p>
-            </div>
-
-          </div>
-        </div>
+           
+                        
+         </div>
+       </div>          
+      </div>
+     </div>
       );
 };
 
