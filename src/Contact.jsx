c08b11580082c325dd84af19260ec3f2a7b3b50f
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-20 05:22:02 +0100

    Added nessesary files for google map, added form with netlify[WIP], changed class to className (syntaxt). deploying for test

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,59 +1,39 @@
-import React from 'react'
-import { Form, Input, TextArea, Button,} from 'semantic-ui-react'
- 
+import React from 'react';
+import { Form, Button,} from 'semantic-ui-react';
+import { MapContainer } from './MapContainer';
+
 
 
 const Contact = () => {
+ 
     return (
         <div className="ui main container">
-          <div class="ui stackable two column grid">
+          <div className="ui stackable two column grid">
 
-            <div class="column">
+            <div className="column">
 
-            <Form inverted data-netlify="true" >
+            <Form name="contact_me" inverted success data-netlify="true" >
+              <input type="hidden" name="contact_me" value="contact" />
                 <Form.Group widths='equal'>
-                  <Form.Field
-                    id='form-input-control-first-name'
-                    control={Input}
-                    label='Name'
-                    placeholder='Name'
-                  />
+
+                <Form.Input name='name' placeholder='Your Name'></Form.Input>
+                <Form.Input name='email' placeholder='Email'></Form.Input>
                 </Form.Group>
-                <Form.Field
-                  id='form-textarea-control-opinion'
-                  control={TextArea}
-                  label='Message'
-                  placeholder='Message'
-                />
-                <Form.Field
-                  id='form-input-control-error-email'
-                  control={Input}
-                  label='Email'
-                  placeholder='email@mail.com'
-                  error={{
-                    content: 'Please enter a valid email address',
-                    pointing: 'below',
-                  }}
-                />
-                <Form.Field
-                  id='form-button-control-public'
-                  control={Button}
-                  content='Confirm'
-                />
+                <Form.Input name='subject' placeholder='Subject'></Form.Input>
+                <Form.Field name='message' placeholder="Your message" control="textarea" rows="10" />
+                <Button type='submit'>Send</Button>
               </Form>   
-                 
             </div>
 
-            <div class="column">
-              <h1 class="ui white inverted header">Contact me</h1>
-              <p>
-              Comimng contact me form
-              </p>
-            </div>
+            
+            <div className="column">
 
+            <p> Map will be here.</p>
+
+            </div>
           </div>
         </div>
       );
 };
 
-export default Contact;
+export default Contact
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Skills = () => {
     return (
         <div className="ui main container">
-          <div class="ui stackable two column grid">
+          <div className="ui stackable two column grid">
 
-            <div class="column">
+            <div className="column">
 
             </div>
 
-            <div class="column">
-            <h1 class="ui white inverted header">Skills</h1>
+            <div className="column">
+            <h1 className="ui white inverted header">Skills</h1>
               <p>
               Skills
               </p>
