d1d26b476aae4f3db48dafecb0dee7bb49f4076e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-14 16:36:16 +0200

    [TEST] added test for contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,9 +10,10 @@ const Contact = () => {
           <h1> Contact me</h1>
           <p>  I'm interested in freelance opportunities, especially for ambitious large projects.
           However, if you have other requests or questions,
-          don't hesitate to contact me using the.
+          don't hesitate to contact me using the form below.
         </p>
         </div>
+        
       </div>
     </FadeIn>
   );
